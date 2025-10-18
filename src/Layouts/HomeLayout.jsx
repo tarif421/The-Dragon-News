93b66c61c24ecda72a8d12dc8c3c1250a03c0e94
Tariful Hasan <tarif305@gmail.com>
2025-10-18 22:52:32 +0600

    marque added

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../Components/Header";
 import { Outlet } from "react-router";
+import LatestNews from "../Components/LatestNews";
 
 const HomeLayout = () => {
   return (
     <div>
       <header>
         <Header></Header>
+        <section className="w-11/12 mx-auto my-3">
+          <LatestNews></LatestNews>
+        </section>
       </header>
       <main>
         <section className="left-nav"></section>
